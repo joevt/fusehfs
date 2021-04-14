ecea5d7b9a480d05eeb3fef63102380e6435bceb
joevt <joevt@shaw.ca>
2021-04-13 20:47:47 -0700

    Updates for macFUSE 4.1.0, macOS 10.15.7, Xcode 12.4
    - Assume the file system will be installed to /Library/ instead of /System/Library/ because /System/ is read-only in macOS 10.15 and later.
    - /sbin/ and /usr/sbin/ are also read-only so we'll use /usr/local/sbin/

--- postinstall.sh
+++ postinstall.sh
@@ -6,5 +6,7 @@
 # Created by Zydeco on 28/5/2010.
 # Copyright 2010 namedfork.net. All rights reserved.
 
-chown root:wheel /System/Library/Filesystems/fusefs_hfs.fs
-ln -fs /System/Library/Filesystems/fusefs_hfs.fs/Contents/Resources/fusefs_hfs /sbin/mount_fusefs_hfs
+chown root:wheel /Library/Filesystems/fusefs_hfs.fs
+sudo mkdir -p /usr/local/sbin
+ln -fs /Library/Filesystems/fusefs_hfs.fs/Contents/Resources/fusefs_hfs /usr/local/sbin/mount_fusefs_hfs
+grep -q /usr/local/sbin /etc/paths || sudo sed -e $'1i\\\n/usr/local/sbin\\\n' -i "" /etc/paths
